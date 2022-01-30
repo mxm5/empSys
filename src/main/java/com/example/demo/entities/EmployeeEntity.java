0f9a9a91bdf05531be0a82bd084408fbb7c5cfb0
mohammadian <mohammadianmx@gmail.com>
2022-01-30 12:32:36 +0330

    initial commit + adding entity Employee and EmployeeModel(dto)

---
+++ src/main/java/com/example/demo/entities/EmployeeEntity.java
@@ -0,0 +1,42 @@
+package com.example.demo.entities;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Table(name = "employee_entity")
+public class EmployeeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        EmployeeEntity that = (EmployeeEntity) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
---
+++ src/main/java/com/example/demo/models/EmployeeModel.java
@@ -0,0 +1,11 @@
+package com.example.demo.models;
+
+import lombok.Data;
+
+@Data
+public class EmployeeModel {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
